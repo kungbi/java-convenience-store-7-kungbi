03db3697f63112ab49beb8b24420ca756f807c4a
kungbi <kipper12335@gmail.com>
2024-11-09 09:02:48 +0900

    refactor(ProductStockService): 구매가 가능하지 않으면 예외를 던짐
    true false 반환에서 예외를 반환하는것으로 변경하였습니다.

--- src/main/java/store/service/ProductStockService.java
+++ src/main/java/store/service/ProductStockService.java
@@ -6,6 +6,9 @@ import store.entity.ProductStock;
 import store.entity.product.Product;
 import store.entity.product.ProductType;
 import store.entity.product.PromotionProduct;
+import store.exception.ProductException;
+import store.exception.ProductStockException;
+import store.exception.message.ProductStockExceptionMessage;
 
 public class ProductStockService {
     private final ProductStock productStock;
@@ -14,48 +17,47 @@ public class ProductStockService {
         this.productStock = productStock;
     }
 
-    public boolean areProductsAvailable(PurchaseItemsDto purchaseItemsDto) {
+    public boolean validateProductsAvailability(PurchaseItemsDto purchaseItemsDto) {
         for (PurchaseItemDto product : purchaseItemsDto.products()) {
-            if (!isProductAvailable(product)) {
-                return false;
+            if (!productStock.isExistProduct(product.name())) {
+                throw new ProductStockException(ProductStockExceptionMessage.NOT_EXIST_PRODUCT);
             }
+
+            validatePromotionProductStock(product);
+            validateCommonProductStock(product);
         }
         return true;
     }
 
     // private methods
 
-    private boolean isProductAvailable(PurchaseItemDto product) {
-        if (!productStock.isExistProduct(product.name())) {
-            return false;
-        }
-
-        if (isPromotionAvailableAndSufficient(product)) {
-            return true;
-        }
-
-        return isCommonProductAvailableAndSufficient(product);
-    }
-
-    private boolean isPromotionAvailableAndSufficient(PurchaseItemDto product) {
+    private void validatePromotionProductStock(PurchaseItemDto product) {
         if (productStock.isExistProductWithType(product.name(), ProductType.PROMOTION)) {
             Product promotionProduct = productStock.getProduct(product.name(), ProductType.PROMOTION);
 
             if (promotionProduct instanceof PromotionProduct promo && promo.isAvailable()) {
-                if (productStock.isSufficientStock(product.name(), ProductType.PROMOTION, product.quantity())) {
-                    return true;
+
+                // 프로모션이 적용 가능하고, 프로모션 상품의 재고가 부족하면서, 일반 상품이 없는 경우
+                // 프로모션이 적용 가능하고, 프로모션 상품의 재고가 부족하면서, 일반 상품의 재고가 부족한 경우
+                if (!productStock.isSufficientStock(product.name(), ProductType.PROMOTION, product.quantity())) {
+                    if (!productStock.isExistProductWithType(product.name(), ProductType.COMMON)) {
+                        throw new ProductStockException(ProductStockExceptionMessage.INSUFFICIENT_STOCK);
+                    }
+
+                    if (!productStock.isSufficientStock(product.name(), ProductType.COMMON, product.quantity())) {
+                        throw new ProductException(ProductStockExceptionMessage.INSUFFICIENT_STOCK);
+                    }
                 }
 
-                return productStock.isExistProductWithType(product.name(), ProductType.COMMON) &&
-                       productStock.isSufficientStock(product.name(), ProductType.COMMON, product.quantity());
             }
         }
-        return false;
     }
 
-    private boolean isCommonProductAvailableAndSufficient(PurchaseItemDto product) {
-        return productStock.isExistProductWithType(product.name(), ProductType.COMMON) &&
-               productStock.isSufficientStock(product.name(), ProductType.COMMON, product.quantity());
+    private void validateCommonProductStock(PurchaseItemDto product) {
+        if (productStock.isExistProductWithType(product.name(), ProductType.COMMON) &&
+            !productStock.isSufficientStock(product.name(), ProductType.COMMON, product.quantity())) {
+            throw new ProductStockException(ProductStockExceptionMessage.INSUFFICIENT_STOCK);
+        }
     }
 
 }
--- src/test/java/store/service/ProductStockServiceTest.java
+++ src/test/java/store/service/ProductStockServiceTest.java
@@ -15,6 +15,9 @@ import store.entity.ProductStock;
 import store.entity.Promotion;
 import store.entity.product.CommonProduct;
 import store.entity.product.PromotionProduct;
+import store.exception.ProductStockException;
+import store.exception.message.ExceptionMessage;
+import store.exception.message.ProductStockExceptionMessage;
 
 class ProductStockServiceTest {
 
@@ -34,25 +37,6 @@ class ProductStockServiceTest {
                             ),
                             true
                     ),
-                    // 구매 요청 상품 중 하나라도 존재하지 않는 경우
-                    Arguments.of(
-                            new PurchaseItemsDto(
-                                    List.of(
-                                            new PurchaseItemDto("콜라", 1),
-                                            new PurchaseItemDto("없는상품", 1)
-                                    )
-                            ),
-                            false
-                    ),
-                    // 일반 상품의 경우, 재고가 부족한 경우
-                    Arguments.of(
-                            new PurchaseItemsDto(
-                                    List.of(
-                                            new PurchaseItemDto("콜라", 100)
-                                    )
-                            ),
-                            false
-                    ),
                     // 적용 가능한 프로모션 상품의 재고가 부족하지만, 일반 상품의 재고는 충분한 경우
                     Arguments.of(
                             new PurchaseItemsDto(
@@ -62,15 +46,6 @@ class ProductStockServiceTest {
                             ),
                             true
                     ),
-                    //  프로모션 상품 적용이 가능하고, 프로모션 상품만 존재하며, 프로모션 상품의 재고가 부족한 경우
-                    Arguments.of(
-                            new PurchaseItemsDto(
-                                    List.of(
-                                            new PurchaseItemDto("펩시", 4)
-                                    )
-                            ),
-                            false
-                    ),
                     // 프로모션 상품 적용이 불가능한데(기간), 일반 상품의 재고가 충분한 경우
                     Arguments.of(
                             new PurchaseItemsDto(
@@ -79,6 +54,39 @@ class ProductStockServiceTest {
                                     )
                             ),
                             true
+                    )
+            );
+        }
+
+        static Stream<Arguments> 예외__상품_구매_가능_테스트_케이스() {
+            return Stream.of(
+                    //  프로모션 상품 적용이 가능하고, 프로모션 상품만 존재하며, 프로모션 상품의 재고가 부족한 경우
+                    Arguments.of(
+                            new PurchaseItemsDto(
+                                    List.of(
+                                            new PurchaseItemDto("펩시", 4)
+                                    )
+                            ),
+                            ProductStockExceptionMessage.INSUFFICIENT_STOCK
+                    ),
+                    // 구매 요청 상품 중 하나라도 존재하지 않는 경우
+                    Arguments.of(
+                            new PurchaseItemsDto(
+                                    List.of(
+                                            new PurchaseItemDto("콜라", 1),
+                                            new PurchaseItemDto("없는상품", 1)
+                                    )
+                            ),
+                            ProductStockExceptionMessage.NOT_EXIST_PRODUCT
+                    ),
+                    // 일반 상품의 경우, 재고가 부족한 경우
+                    Arguments.of(
+                            new PurchaseItemsDto(
+                                    List.of(
+                                            new PurchaseItemDto("콜라", 100)
+                                    )
+                            ),
+                            ProductStockExceptionMessage.INSUFFICIENT_STOCK
                     ),
                     // 프로모션 상품 적용이 불가능한데(기간), 일반 상품의 재고가 부족한 경우
                     Arguments.of(
@@ -87,7 +95,7 @@ class ProductStockServiceTest {
                                             new PurchaseItemDto("밀키스", 11)
                                     )
                             ),
-                            false
+                            ProductStockExceptionMessage.INSUFFICIENT_STOCK
                     )
             );
         }
@@ -111,15 +119,29 @@ class ProductStockServiceTest {
 
         @ParameterizedTest
         @MethodSource("정상__상품_구매_가능_테스트_케이스")
-        void 정상__상품_구매_가능(PurchaseItemsDto purchaseItemsDto, boolean expected) {
+        void 정상__상품_구매_가능(PurchaseItemsDto purchaseRequestDto, boolean expected) {
             // given
             ProductStockService productStockService = new ProductStockService(productStock);
 
             // when
-            boolean result = productStockService.areProductsAvailable(purchaseItemsDto);
+            boolean result = productStockService.validateProductsAvailability(purchaseRequestDto);
 
             // then
             Assertions.assertEquals(expected, result);
+        }
+
+        @ParameterizedTest
+        @MethodSource("예외__상품_구매_가능_테스트_케이스")
+        void 예외__상품_구매_가능_테스트(PurchaseItemsDto purchaseItemsDto, ExceptionMessage message) {
+            // given
+            ProductStockService productStockService = new ProductStockService(productStock);
+
+            // when
+            ProductStockException exception = Assertions.assertThrows(ProductStockException.class,
+                    () -> productStockService.validateProductsAvailability(purchaseItemsDto));
+
+            // then
+            Assertions.assertEquals(message.getMessage(), exception.getMessage());
         }
     }
 }
