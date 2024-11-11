0b8c09dfa302c9477aa4386d778cdc4058efdc68
kungbi <kipper12335@gmail.com>
2024-11-11 18:13:29 +0900

    refactor(PurchaseService): 메소드 분리

--- src/main/java/store/service/PurchaseService.java
+++ src/main/java/store/service/PurchaseService.java
@@ -7,7 +7,6 @@ import store.dto.PurchaseItemsDto;
 import store.dto.PurchaseRequestDto;
 import store.dto.PurchaseResultDto;
 import store.dto.PurchaseResultItemDto;
-import store.entity.Promotion;
 import store.entity.membership.Membership;
 import store.entity.product.Product;
 import store.entity.product.ProductType;
@@ -26,51 +25,58 @@ public class PurchaseService {
     }
 
     public PurchaseResultDto purchase(PurchaseRequestDto purchaseInputDto) {
-        productStockService.validateStocks(new PurchaseItemsDto(
-                purchaseInputDto.products()
-        ));
-
+        productStockService.validateStocks(new PurchaseItemsDto(purchaseInputDto.products()));
         List<ItemDto> freeItems = new ArrayList<>();
-        List<PurchaseResultItemDto> purchaseItems = new ArrayList<>();
-        for (ItemDto product : purchaseInputDto.products()) {
-            if (productStockService.isExistProductWithType(product.name(), ProductType.PROMOTION) && productStockService.getProduct(product.name(), ProductType.PROMOTION).getType()
-                != ProductType.PROMOTION) {
-                continue;
-            }
-
-            ItemDto excludedPromotionItem = promotionService.findExcludedPromotionItem(product);
-            int freeCount = promotionService.calculateFreeCount(
-                    new ItemDto(product.name(), product.quantity() - excludedPromotionItem.quantity()));
-            if (freeCount > 0) {
-                freeItems.add(new ItemDto(product.name(), freeCount));
-            }
-
-            purchaseItems.add(
-                    new PurchaseResultItemDto(product.name(), product.quantity(), calculateItemPrice(product)));
-        }
+        List<PurchaseResultItemDto> purchaseItems = calculatePurchaseItems(purchaseInputDto, freeItems);
 
         int totalAmount = calculateItemsTotalPrice(purchaseInputDto.products());
-        int promotionDiscountAmount = freeItems.stream()
-                .reduce(0, (sum, item) -> sum +
-                                          productStockService.getProduct(item.name(), ProductType.PROMOTION).getPrice()
-                                          * item.quantity(), Integer::sum);
+        int promotionDiscountAmount = calculatePromotionDiscountAmount(freeItems);
+        int membershipDiscountAmount = calculateMembershipDiscountAmount(purchaseInputDto, totalAmount);
+        this.productStockService.reduceStocks(new PurchaseItemsDto(purchaseInputDto.products()));
+        return createPurchaseResultDto(purchaseItems, freeItems, promotionDiscountAmount, membershipDiscountAmount,
+                totalAmount);
+    }
+
+    private int calculateMembershipDiscountAmount(PurchaseRequestDto purchaseInputDto, int totalAmount) {
         int membershipDiscountAmount = 0;
         if (purchaseInputDto.isMembership()) {
             membershipDiscountAmount = membership.applyDiscount(totalAmount);
         }
+        return membershipDiscountAmount;
+    }
 
-        this.productStockService.reduceStocks(new PurchaseItemsDto(
-                purchaseInputDto.products()
-        ));
+    private Integer calculatePromotionDiscountAmount(List<ItemDto> freeItems) {
+        return freeItems.stream().reduce(0, (sum, item) -> sum + productStockService.getProduct(item.name(),
+                ProductType.PROMOTION).getPrice() * item.quantity(), Integer::sum);
+    }
 
-        return new PurchaseResultDto.Builder()
-                .purchaseItems(purchaseItems)
-                .freeItems(freeItems)
-                .promotionDiscountAmount(promotionDiscountAmount)
-                .membershipDiscountAmount(membershipDiscountAmount)
-                .totalAmount(totalAmount)
-                .paymentAmount(totalAmount - promotionDiscountAmount - membershipDiscountAmount)
-                .build();
+    private List<PurchaseResultItemDto> calculatePurchaseItems(PurchaseRequestDto purchaseInputDto,
+                                                               List<ItemDto> freeItems) {
+        List<PurchaseResultItemDto> purchaseItems = new ArrayList<>();
+        for (ItemDto product : purchaseInputDto.products()) {
+            calculatePurchaseItem(freeItems, product, purchaseItems);
+        }
+        return purchaseItems;
+    }
+
+    private void calculatePurchaseItem(List<ItemDto> freeItems, ItemDto product,
+                                       List<PurchaseResultItemDto> purchaseItems) {
+        if (validateProduct(product)) {
+            return;
+        }
+        ItemDto excludedPromotionItem = promotionService.findExcludedPromotionItem(product);
+        int freeCount = promotionService.calculateFreeCount(
+                new ItemDto(product.name(), product.quantity() - excludedPromotionItem.quantity()));
+        if (freeCount > 0) {
+            freeItems.add(new ItemDto(product.name(), freeCount));
+        }
+        purchaseItems.add(new PurchaseResultItemDto(product.name(), product.quantity(), calculateItemPrice(product)));
+    }
+
+    private boolean validateProduct(ItemDto product) {
+        return productStockService.isExistProductWithType(product.name(), ProductType.PROMOTION)
+               && productStockService.getProduct(product.name(), ProductType.PROMOTION).getType()
+                  != ProductType.PROMOTION;
     }
 
     public int calculateItemsTotalPrice(List<ItemDto> items) {
@@ -83,43 +89,62 @@ public class PurchaseService {
 
     public int calculateItemPrice(ItemDto item) {
         String productName = item.name();
+
         int purchaseQuantity = item.quantity();
+        int promotionPrice = getPromotionPrice(productName);
+        int commonPrice = getCommonPrice(productName);
+        int availablePromotionStock = getAvailablePromotionStock(productName, promotionPrice);
+        int promotionQuantity = calculatePromotionQuantity(purchaseQuantity, availablePromotionStock);
+        int commonQuantity = calculateCommonQuantity(purchaseQuantity, promotionQuantity);
 
-        // 프로모션 상품과 일반 상품의 가격을 가져옵니다.
-        int promotionPrice = 0;
-        int commonPrice = 0;
+        return calculateTotalPrice(promotionQuantity, promotionPrice, commonQuantity, commonPrice);
+    }
 
-        // 프로모션 상품이 존재하고 적용 가능한지 확인
+    private int getPromotionPrice(String productName) {
         if (productStockService.isExistProductWithType(productName, ProductType.PROMOTION)) {
             Product promotionProduct = productStockService.getProduct(productName, ProductType.PROMOTION);
-
-            // 프로모션이 적용 가능한 경우에만 프로모션 가격 설정
-            if (promotionProduct instanceof PromotionProduct promoProduct) {
-                Promotion promotion = promoProduct.getPromotion();
-                if (promotion.isAvailable()) {
-                    promotionPrice = promoProduct.getPrice();
-                }
+            if (promotionProduct instanceof PromotionProduct promoProduct && promoProduct.getPromotion()
+                    .isAvailable()) {
+                return promoProduct.getPrice();
             }
         }
+        return 0;
+    }
 
-        // 일반 상품 가격 설정
+    private int getCommonPrice(String productName) {
         if (productStockService.isExistProductWithType(productName, ProductType.COMMON)) {
             Product commonProduct = productStockService.getProduct(productName, ProductType.COMMON);
-            commonPrice = commonProduct.getPrice();
+            return commonProduct.getPrice();
         }
+        return 0;
+    }
 
-        // 사용할 수 있는 프로모션 수량 확인 (프로모션 가격이 설정된 경우에만)
-        int availablePromotionStock = 0;
+    private int getAvailablePromotionStock(String productName, int promotionPrice) {
         if (promotionPrice > 0) {
-            availablePromotionStock = productStockService.getProductQuantity(productName, ProductType.PROMOTION);
+            return productStockService.getProductQuantity(productName, ProductType.PROMOTION);
         }
+        return 0;
+    }
 
-        // 프로모션 상품과 일반 상품의 수량을 계산
-        int promotionQuantity = Math.min(purchaseQuantity, availablePromotionStock);
-        int commonQuantity = purchaseQuantity - promotionQuantity;
+    private int calculatePromotionQuantity(int purchaseQuantity, int availablePromotionStock) {
+        return Math.min(purchaseQuantity, availablePromotionStock);
+    }
 
-        // 최종 가격 계산
+    private int calculateCommonQuantity(int purchaseQuantity, int promotionQuantity) {
+        return purchaseQuantity - promotionQuantity;
+    }
+
+    private int calculateTotalPrice(int promotionQuantity, int promotionPrice, int commonQuantity, int commonPrice) {
         return (promotionQuantity * promotionPrice) + (commonQuantity * commonPrice);
+    }
+
+    private static PurchaseResultDto createPurchaseResultDto(List<PurchaseResultItemDto> purchaseItems,
+                                                             List<ItemDto> freeItems, int promotionDiscountAmount,
+                                                             int membershipDiscountAmount, int totalAmount) {
+        return new PurchaseResultDto.Builder().purchaseItems(purchaseItems).freeItems(freeItems)
+                .promotionDiscountAmount(promotionDiscountAmount).membershipDiscountAmount(membershipDiscountAmount)
+                .totalAmount(totalAmount)
+                .paymentAmount(totalAmount - promotionDiscountAmount - membershipDiscountAmount).build();
     }
 
 
