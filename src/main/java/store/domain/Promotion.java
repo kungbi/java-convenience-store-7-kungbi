1ca54d250bb0abff35ec025a1be046c0b910db05
kungbi <kipper12335@gmail.com>
2024-11-17 16:34:39 +0900

    feat(domain): 프로모션 entity 생성

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,41 @@
+package store.domain;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+
+    public Promotion(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+        validate(name, buy, get, startDate, endDate);
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private void validate(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        if (buy <= 0) {
+            throw new IllegalArgumentException("Buy cannot be negative");
+        }
+        if (get <= 0) {
+            throw new IllegalArgumentException("Get cannot be negative");
+        }
+        if (startDate == null) {
+            throw new IllegalArgumentException("Start date cannot be null");
+        }
+        if (endDate == null) {
+            throw new IllegalArgumentException("End date cannot be null");
+        }
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start date cannot be after end date");
+        }
+    }
+}
